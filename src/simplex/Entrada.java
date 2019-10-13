8769d8d2bd67c186d075d3a99748638a92e1e8a1
David Rodrigues <davidrod.contato@gmail.com>
2019-10-13 16:58:31 -0300

    nao sei pq n ta indo to puto

--- src/simplex/Entrada.java
+++ src/simplex/Entrada.java
@@ -13,7 +13,7 @@ public class Entrada {
     private Scanner in = new Scanner(System.in);
     private String holder;
     private int numeroDeCariaveis;
-    double restricoes[][];
+    public double restricoes[][];
 
    
     
@@ -26,13 +26,20 @@ public class Entrada {
         setNumeroDeCariaveis(var);
         System.out.println("Informe os coeficientes das seguintes variaveis para gerar a funcao objetiva:");
         for(int i = 0; i<= numeroDeCariaveis; i++){
-            System.out.println("Informe o coeficiente de X"+i);
+            System.out.println("Informe o coeficiente de X"+(i+1));
             holder = in.nextLine();
-            restricoes[0][i] = Integer.parseInt(holder);         
+            double oi = Double.parseDouble(holder);      
+            restricoes[0][i]=oi;  
         }
-        System.out.println("");
+        System.out.println("Sua função objetiva e: \n");
+            for(int i = 0; i<= numeroDeCariaveis; i++){
+                if(restricoes[0][i]<0){
+                System.out.println("-"+restricoes[0][i]+"X"+(i+1));
+            }else{
+                    System.out.println("+"+restricoes[0][i]+"X"+(i+1));
+                }
+        }
     }
-        
     
     
     public int getNumeroDeCariaveis() {
